4e1ad9d2b37e8f2ffcc37dbf3238c836999fae26
Amith B V <amithrao0606@gmail.com>
2025-08-26 20:11:03 +0530

    Created an Express Server.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Server running at port 123!!! Amith");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Management System");
+});
+
+app.listen(3000, () => {
+  console.log(
+    "Subscription Management System API is running on http://localhost:3000/"
+  );
+});
+
+export default app;
