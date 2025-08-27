d2e07a74ffce82ce664447505a13c809035a2111
Amith B V <amithrao0606@gmail.com>
2025-08-27 17:35:06 +0530

    Error middlewares.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
+// Built in middlewares :
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // To processing form data
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Management System");
