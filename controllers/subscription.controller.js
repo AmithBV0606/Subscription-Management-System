6efe7d808e166bc23328985b38a83cb2bd520098
Amith B V <amithrao0606@gmail.com>
2025-08-28 17:59:40 +0530

    CRUD operation on Subscription model.

---
+++ controllers/subscription.controller.js
@@ -0,0 +1,77 @@
+import Subscription from "../models/subscription.model.js";
+
+export const createSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    const subscriptions = await Subscription.find({
+      user: req.user._id,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: subscriptions,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserSubscription = async (req, res, next) => {
+  try {
+    const updateSubscription = await Subscription.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { returnDocument: "after" }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: updateSubscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUserSubscription = async (req, res, next) => {
+  try {
+    const deleteSubscription = await Subscription.findByIdAndDelete(
+      req.params.id
+    );
+
+    res.status(200).json({
+      success: true,
+      data: deleteSubscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,46 +1,22 @@
 import { Router } from "express";
+import authorize from "../middleware/auth.middleware.js";
+import {
+  createSubscription,
+  deleteUserSubscription,
+  getUserSubscription,
+  getUserSubscriptions,
+  updateUserSubscription,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 // All subscription routes :
-subscriptionRouter.get("/", (req, res) => {
-  res.send({ title: "GET all subscriptions." });
-});
-
-subscriptionRouter.post("/", (req, res) => {
-  res.send({ title: "Create a subscriptions." });
-});
+subscriptionRouter.get("/", authorize, getUserSubscriptions);
+subscriptionRouter.post("/", authorize, createSubscription);
 
 // Individual subscription routes :
-subscriptionRouter.get("/:id", (req, res) => {
-  res.send({ title: "GET an individual subscription details." });
-});
-
-subscriptionRouter.put("/:id", (req, res) => {
-  res.send({ title: "PUT(Update) a subscriptions detail." });
-});
-
-subscriptionRouter.delete("/:id", (req, res) => {
-  res.send({ title: "DELETE(Remove) a subscriptions detail." });
-});
-
-// Fetch the subscription details of a particular user :
-subscriptionRouter.get("/user/:id", (req, res) => {
-  res.send({
-    title: "GET(Read) the subscriptions details of a specified user.",
-  });
-});
-
-subscriptionRouter.put("/:id/cancel", (req, res) => {
-  res.send({
-    title: "Cancel the subscriptions of a specified user.",
-  });
-});
-
-subscriptionRouter.get("/upcoming-renewals", (req, res) => {
-  res.send({
-    title: "Upgrade the subscription.",
-  });
-});
+subscriptionRouter.get("/:id", authorize, getUserSubscription);
+subscriptionRouter.put("/:id", authorize, updateUserSubscription);
+subscriptionRouter.delete("/:id", authorize, deleteUserSubscription);
 
 export default subscriptionRouter;
