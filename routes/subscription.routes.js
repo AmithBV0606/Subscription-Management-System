d64d34d78eabb7d6d55c1171e277b45e22dd8999
Amith B V <amithrao0606@gmail.com>
2025-08-28 22:30:07 +0530

    Bought back some deleted routes.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -19,4 +19,23 @@ subscriptionRouter.get("/:id", authorize, getUserSubscription);
 subscriptionRouter.put("/:id", authorize, updateUserSubscription);
 subscriptionRouter.delete("/:id", authorize, deleteUserSubscription);
 
+// Fetch the subscription details of a particular user :
+subscriptionRouter.get("/user/:id", (req, res) => {
+  res.send({
+    title: "GET(Read) the subscriptions details of a specified user.",
+  });
+});
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({
+    title: "Cancel the subscriptions of a specified user.",
+  });
+});
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({
+    title: "Upgrade the subscription.",
+  });
+});
+
 export default subscriptionRouter;
