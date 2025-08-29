76e91d929329e349978a5acce10b2596d0d28198
Amith B V <amithrao0606@gmail.com>
2025-08-29 08:32:03 +0530

    Reminder workflow.

---
+++ controllers/workflow.controller.js
@@ -0,0 +1,65 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { serve } = require("@upstash/workflow/express");
+
+import dayjs from "dayjs";
+import Subscription from "../models/subscription.model.js";
+
+const REMINDERS = [7, 5, 2, 1];
+
+export const sendRemainders = serve(async (context) => {
+  // When we trigger a workflow we need to pass in the subscription id that workflow is for :
+  const { subscriptionId } = context.requestPayload;
+
+  const subscription = await fetchSubscription(context, subscriptionId);
+
+  if (!subscription || subscription.status !== "active") return;
+
+  const renewalDate = dayjs(subscription.renewalDate);
+
+  if (renewalDate.isBefore(dayjs())) {
+    console.log(
+      `Renewal date has passed for subscription ${subscriptionId}. Stopping the workflow.`
+    );
+    return;
+  }
+
+  for (const daysBefore of REMINDERS) {
+    const reminderDate = renewalDate.subtract(daysBefore, "day");
+
+    // Figuring out when to sleep and when to send email :
+    if (reminderDate.isAfter(dayjs())) {
+      await sleepUntilReminder(
+        context,
+        `Reminder ${daysBefore} days before`,
+        reminderDate
+      );
+    }
+
+    if (dayjs().isSame(reminderDate, "day")) {
+      await triggerReminder(
+        context,
+        `${daysBefore} days before reminder`,
+        subscription
+      );
+    }
+  }
+});
+
+const fetchSubscription = async (context, subscriptionId) => {
+  return await context.run("get subscription", async () => {
+    return Subscription.findById(subscriptionId).populate("user", "name email");
+  });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+  console.log(`Sleeping until ${label} reminder at ${date}`);
+  await context.sleepUntil(label, date.toDate());
+};
+
+const triggerReminder = async (context, label) => {
+  return await context.run(label, () => {
+    console.log(`Triggering ${label} remainder.`);
+    // Send emai, sms, etc
+  });
+};
